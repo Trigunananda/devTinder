4259e380084066bac4d8d5f243a205610c1f18a9
Triguna <swaint214@gmail.com>
2024-09-22 00:10:18 +0530

    Exploring GET POST DELETE API call

---
+++ homework.md
@@ -0,0 +1,20 @@
+- Create a repository
+- Initialize the repository
+- node_modules, package.json, package-lock.json
+- Install express
+- Create a server
+- Listen to port 7777
+- Write request handlers for /test , /hello
+- Install nodemon and update scripts inside package.json
+- What are dependencies
+- What is the use of "-g" while npm install
+- Difference between caret and tilde ( ^ vs ~ )
+
+- Intialize list
+- .gitignore
+- create a remote repo on github
+- push all code to remote origin
+- play with routes and route extension ex. /hello, / , hello/2, /xyz
+- Orders of the routes matter a lot
+- Install a postman app and make a workspace/collection > test API call
+- Write logic to handle GET POST DELETE API call and test into POST man
--- src/app.js
+++ src/app.js
@@ -1,16 +1,23 @@
 const express = require("express")
 
 const app = express()
-app.use("/",(req,res)=>{
-    res.send("Namaste Triguna");
-    });
-app.use("/hello",(req,res)=>{
-    res.send("Hello Hello Hello");
-    });
-//request Handler
-app.use("/test",(req,res)=>{
-res.send("Hello from the server");
+app.get("/user", (req, res) => {
+    res.send({ firstName: "Triguna", lastName: "Swain" });
 });
-app.listen(7777,()=>{
+app.post("/user", (req, res) => {
+    console.log("Save Data to the database")
+    res.send("Data successfully saved to the database");
+});
+app.delete("/user", (req, res) => {
+    res.send("Data deleted successfully");
+});
+//request Handler
+app.use("/test", (req, res) => {
+    res.send("Hello from the server");
+});
+// app.use("/",(req,res)=>{
+//     res.send("Namaste Triguna");
+//     });
+app.listen(7777, () => {
     console.log("Server is successfully listening on port 7777...")
 })
