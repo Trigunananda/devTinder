f4cedadec49e90dc7f0e1cb43184a06601e392d8
Triguna <swaint214@gmail.com>
2024-09-21 17:51:05 +0530

    create a express server

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const express = require("express")
+
+const app = express()
+app.use("/",(req,res)=>{
+    res.send("Namaste Triguna");
+    });
+app.use("/hello",(req,res)=>{
+    res.send("Hello Hello Hello");
+    });
+//request Handler
+app.use("/test",(req,res)=>{
+res.send("Hello from the server");
+});
+app.listen(7777,()=>{
+    console.log("Server is successfully listening on port 7777...")
+})
